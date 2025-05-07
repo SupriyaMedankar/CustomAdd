c6c779d09fb3e0fdf4b1dc51f1b4a3ccb4f3578a
SupriyaMedankar <supriya6332@gmail.com>
2025-05-07 21:12:58 +0530

    add comments in code

--- add.rb
+++ add.rb
@@ -4,17 +4,23 @@ def add(str)
   # Check if the string is empty
   return 0 if str.empty?
 
-  # Split the string by commas and convert each part to an integer
   # Check if the string starts with a custom delimiter
   if str.start_with?("//")
     delimiter, str = str.split("\n", 2)
+
+    # Remove the leading "//" and split the delimiter string
     delimiters = delimiter.gsub("//", "").split(/[\[\]]+/).reject(&:empty?)
     delimiter = Regexp.union(delimiters)
   else
     delimiter = /[,\n]/
   end
 
+  # Split the string using the delimiter
+  # Convert the string to an array of integers
+  # Reject numbers greater than 1000
   nums = str.split(delimiter).map(&:to_i).reject { |num| num > 1000 }
+
+  # Check for negative numbers
   negatives = nums.select { |num| num < 0 }
 
   raise "negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
