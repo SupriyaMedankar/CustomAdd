bfa67b6aace026b3ff34502f79854bbf815a4d4c
SupriyaMedankar <supriya6332@gmail.com>
2025-05-12 16:48:58 +0530

    refactor

--- add.rb
+++ add.rb
@@ -4,6 +4,8 @@ def add(str)
   # Check if the string is empty
   return 0 if str.empty?
 
+  delimiter = /,|\n/
+
   # Check if the string starts with a custom delimiter
   if str.start_with?("//")
     delimiter, str = str.split("\n", 2)
@@ -11,8 +13,6 @@ def add(str)
     # Remove the leading "//" and split the delimiter string
     delimiters = delimiter.gsub("//", "").split(/[\[\]]+/).reject(&:empty?)
     delimiter = Regexp.union(delimiters)
-  else
-    delimiter = /[,\n]/
   end
 
   # Split the string using the delimiter
